03467961e0a3780a618a0984cb6812ac48f9ab36
Atharva <atharvaskysandyadav@gmail.com>
2021-01-06 13:00:28 +0530

    projectory

--- Bird.js
+++ Bird.js
@@ -2,6 +2,8 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeimage = loadImage("sprites/smoke.png");
+    this.projectory = []
   }
 
   display() {
@@ -9,5 +11,13 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+    if(this.body.velocity.x>10 && this.body.position.x>200){
+      var positions = [this.body.position.x,this.body.position.y];
+      this.projectory.push(positions);
+
+    }
+    for (var i = 0; i<this.projectory.length;i++){
+      image(this.smokeimage,this.projectory[i][0],this.projectory[i][1]);
+    }
   }
 }
